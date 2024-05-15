301473119959cd5e4634ef85a471c4cedcbc755d
ethan <dickvla@gmail.com>
2024-05-15 12:22:06 +1200

    Co-authored-by: dickvla <dickvla@users.noreply.github.com> show game result

--- scripts/game.js
+++ scripts/game.js
@@ -2,8 +2,12 @@ const keyboardDiv = document.querySelector(".keyboard");
 const wordDisplay = document.querySelector(".word-display");
 const hangmanBox = document.querySelector(".hangman-box");
 const hangmanBoxImage = document.querySelector(".hangman-box img");
+const gameModal = document.querySelector(".game-modal");
+
 const word = 'banana';
 let incorrect = 0;
+let correctLettersShown = 0;
+const maxNumberOfGuesses = 6;
 
 const fruits = ['apple', 'banana', 'orange', 'kiwifruit', 'mandarin']
 
@@ -37,26 +41,46 @@ function showKeyboard() {
 }
 
 function useKeyboard(evt) {
+    // disable letter on keyboard
+    evt.target.disabled = true;
     // show letter clicked
     const letter = evt.target.innerHTML.toLowerCase();
     console.log(letter);
     // check if letter is in word
     if (word.includes(letter)) {
         // Yes it is
+        
 
         // loop over all letters in word
         for(let i=0;i<word.length; i++){
             // does the letter clicked match the letter in the word?
             if (word[i] === letter){
+                correctLettersShown++;
                 // Yes, show the letter
                 wordDisplay.querySelectorAll("li")[i].innerText = letter;
+                if (correctLettersShown === word.length ){
+                    gameOver(true);
+                }
             }
         }
     } else {
         // The letter is not in the word
         incorrect++;
-        hangmanBoxImage.src = `assets/images/hangman-${incorrect}.svg`
+        hangmanBoxImage.src = `assets/images/hangman-${incorrect}.svg`;
+        if (incorrect === maxNumberOfGuesses) {
+            gameOver(false);
+        }
     }
 }
+
+const gameOver = (isVictory) => {
+    // After game complete.. showing modal with relevant details
+    const modalText = isVictory ? `You found the word:` : 'The correct word was:';
+    gameModal.querySelector("img").src = `assets/images/${isVictory ? 'victory' : 'lost'}.gif`;
+    gameModal.querySelector("h4").innerText = isVictory ? 'Congrats!' : 'Game Over!';
+    gameModal.querySelector("p").innerHTML = `${modalText} <b>${word}</b>`;
+    gameModal.classList.add("show");
+}
+
 showPlaceForWord() // do it
 showKeyboard()
